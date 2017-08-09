b28fe1d95867820dd9875c24f954fab37c73081b
Sahithi Boda <sahithib27@gmail.com>
2017-08-09 15:28:22 -0500

    added method12

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         hello3();
     }
     public static void hello(){
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!!");
     }
 
     public static void hello1(){
